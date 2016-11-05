ed644c0e3f6d6af71ccc2001f479c0ddbd761aaa
puu <puu_talo@i.softbank.jp>
2016-11-06 00:28:33 +0900

    add bias

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,14 +8,15 @@ type Tensor = [Matrix R]
 type Layer = [Int]
 
 genTensor :: Layer -> IO Tensor
-genTensor ns = mapM (\(x, y) -> uncurry randn (x, y)) ts
+genTensor ns = mapM (\(x, y) -> uncurry randn (x, y + 1)) ts -- 1: bias
   where ts = drop 1 $ zip ns (0:ns)
 
 forwards :: Tensor -> Matrix R -> Matrix R
 forwards t vs = foldr forward vs $ reverse t
 
 forward :: Matrix R -> Matrix R -> Matrix R
-forward m vs = sigmoid $ m <> vs
+forward m vs = sigmoid $ m <> vbs
+  where vbs = fromLists $ toLists vs `mappend` [replicate (cols vs) 1] -- [1,1,..1]: bias
 
 sigmoid n = 1 / (1 + exp (-n))
 
