20384126c9e449f4624e85c6fb9efea8e788f28c
puu <puu_talo@i.softbank.jp>
2016-11-05 23:22:53 +0900

    implement forward

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,22 @@ module Lib
     ( someFunc
     ) where
 
+import Numeric.LinearAlgebra
+
+type Tensor = [Matrix R]
+type Layer = [Int]
+
+genTensor :: Layer -> IO Tensor
+genTensor ns = mapM (\(x, y) -> uncurry randn (x, y)) ts
+  where ts = drop 1 $ zip ns (0:ns)
+
+forwards :: Tensor -> Vector R -> Vector R
+forwards t v = foldr forward v $ reverse t
+
+forward :: Matrix R -> Vector R -> Vector R
+forward m v = sigmoid $ m #> v
+
+sigmoid n = 1 / (1 + exp (-n))
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
