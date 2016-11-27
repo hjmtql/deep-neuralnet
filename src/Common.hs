0fa614a3213f54175e1d4ec3c74375dccdb3a140
puu <puu_talo@i.softbank.jp>
2016-11-27 12:50:16 +0900

    change initial weight random to 0

--- src/Common.hs
+++ src/Common.hs
@@ -13,7 +13,8 @@ genWeights ns = mapM genWeight ts
   where ts = zip ns (tail ns)
 
 genWeight :: (Int, Int) -> IO (Matrix R)
-genWeight (i, o) = randn o (i + 1) -- +1: bias
+-- genWeight (i, o) = randn o (i + 1) -- +1: bias
+genWeight (i, o) = return . matrix (i + 1) . replicate (o * (i + 1)) $ 0 -- +1: bias
 
 inputWithBias :: Matrix R -> Matrix R
 inputWithBias v = v === konst 1 (1, cols v) -- [1,1,..1]: bias
