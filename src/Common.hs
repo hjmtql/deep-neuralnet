c32a517194e11a40a686c07ade27517a4d728f72
puu <puu_talo@i.softbank.jp>
2016-11-29 01:25:14 +0900

    add initWeights

--- src/Common.hs
+++ src/Common.hs
@@ -1,5 +1,6 @@
 module Common (
     genWeights,
+    initWeights,
     inputWithBias,
     weightWithoutBias,
     pickupSets
@@ -13,8 +14,14 @@ genWeights ns = mapM genWeight ts
   where ts = zip ns (tail ns)
 
 genWeight :: (Int, Int) -> IO (Matrix R)
--- genWeight (i, o) = randn o (i + 1) -- +1: bias
-genWeight (i, o) = return . matrix (i + 1) . replicate (o * (i + 1)) $ 0 -- +1: bias
+genWeight (i, o) = randn o (i + 1) -- +1: bias
+
+initWeights :: [Int] -> [Matrix R]
+initWeights ns = fmap initWeight ts
+  where ts = zip ns (tail ns)
+
+initWeight :: (Int, Int) -> Matrix R
+initWeight (i, o) = matrix (i + 1) . replicate (o * (i + 1)) $ 0 -- +1: bias
 
 inputWithBias :: Matrix R -> Matrix R
 inputWithBias v = v === konst 1 (1, cols v) -- [1,1,..1]: bias
